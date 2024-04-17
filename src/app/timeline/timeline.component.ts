177493bb667eb614d5a0b3e8469c4b31be7d776a
splearn1 <barescents4u@gmail.com>
2024-04-17 17:45:34 -0400

    deleted unused timeline service and model

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Timeline } from '../models/timeline';
-import { TimelineService } from '../services/timeline.service';
 import { User } from '../models/users';
 import { UserService } from '../services/user.service';
 import { Pet } from '../models/pets';
@@ -16,12 +14,9 @@ import { PetService } from '../services/pet.service';
   styleUrl: './timeline.component.scss'
 })
 export class TimelineComponent implements OnInit {
-  timelines: Timeline[] = [];
-  newTimelineBody: string = '';
-  users: User[] = [];
   currentUser: User | null = null;
 
-  constructor(private timelineService: TimelineService, private userService:UserService, private petService: PetService) { }
+  constructor(private userService:UserService, private petService: PetService) { }
 
   ngOnInit(): void {
     this.userService.currentUserBehaviorSubject.subscribe((user) => {
